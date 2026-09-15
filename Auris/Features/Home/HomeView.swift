9b0302af4af0702d0d6597ed2ef5a54a67074961
Marcel Piešťanský <marcel@piestansky.cz>
2026-09-15 14:25:12 +0200

    Added preview content

--- Auris/Features/Home/HomeView.swift
+++ Auris/Features/Home/HomeView.swift
@@ -50,7 +50,18 @@ struct HomeView: View {
         func load() async throws -> HomeContent {
             try await Task.sleep(nanoseconds: 1_000_000_000)
             
-            return .unauth
+            return .library([
+                MusicItem(id: "song:1", type: .song, title: "I Bet You Look Good on the Dancefloor", subtitle: "Arctic Monkeys • 3m 38s", imageURL: nil),
+                MusicItem(id: "song:2", type: .song, title: "Dreams", subtitle: "Fleetwood Mac • 4m 18s", imageURL: nil),
+                MusicItem(id: "song:3", type: .song, title: "Midnight City", subtitle: "M83 • 4m 3s", imageURL: nil),
+                MusicItem(id: "song:4", type: .song, title: "Redbone", subtitle: "Childish Gambino • 5m 27s", imageURL: nil),
+                MusicItem(id: "album:1", type: .album, title: "AM", subtitle: "Arctic Monkeys • 12 tracks", imageURL: nil),
+                MusicItem(id: "album:2", type: .album, title: "Rumours", subtitle: "Fleetwood Mac • 11 tracks", imageURL: nil),
+                MusicItem(id: "album:3", type: .album, title: "Random Access Memories", subtitle: "Daft Punk • 13 tracks", imageURL: nil),
+                MusicItem(id: "playlist:1", type: .playlist, title: "Morning Focus", subtitle: "Apple Music", imageURL: nil),
+                MusicItem(id: "playlist:2", type: .playlist, title: "Late Night Drive", subtitle: "Apple Music", imageURL: nil),
+                MusicItem(id: "playlist:3", type: .playlist, title: "Discover Mix", subtitle: "Apple Music", imageURL: nil)
+            ])
         }
     }
     struct MockMusicAuthorizationService: MusicAuthorizationService {
